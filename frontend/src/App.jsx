f210b9899e65ea8bac613b371077468d025efa1f
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2025-07-06 21:11:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
-const App = () => {
+const App = ( ) => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   const { theme } = useThemeStore();
 
